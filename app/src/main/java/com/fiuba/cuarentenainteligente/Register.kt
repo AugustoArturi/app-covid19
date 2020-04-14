c83b5fc8fa09eb799722c5dc53e9434a062bdda8
Augusto Arturi <arturiaugusto@gmail.com>
2020-04-14 18:16:56 -0300

    adding push notifications, base

---
+++ app/src/main/java/com/fiuba/cuarentenainteligente/Register.kt
@@ -0,0 +1,7 @@
+package com.fiuba.cuarentenainteligente
+
+class UserRegister (
+    var messageToken:String,
+    var dni:String,
+    var state:String
+)
---
+++ app/src/main/java/com/fiuba/cuarentenainteligente/User.kt
@@ -0,0 +1,8 @@
+package com.fiuba.cuarentenainteligente
+
+class User (
+    var dni: String,
+    var contactDni:String,
+    var time:String
+
+)
